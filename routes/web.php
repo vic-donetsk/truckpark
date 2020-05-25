0fdef04b3e7ac576f4823e5ddcae93fc2737473e
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2020-05-25 17:06:16 +0300

    #routes -grouping

--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-
 Auth::routes();
 
 Route::get('/', 'ParkController@index')->name('index')->middleware('auth');
 
-Route::get('/parks', 'ParkController@show')->name('park_show')->middleware('auth','can:all-parks');
+// Менеджер
+Route::middleware(['auth','can:all-parks'])->group(function () {
+    // показать все парки
+    Route::get('/parks', 'ParkController@show')->name('park_show');
+    // создать/редактировать парка
+    Route::get('/park_edit/{id?}', 'ParkController@edit')->name('park_edit');
+    // сохранение парка
+    Route::post('/park_update', 'ParkController@update')->name('park_update');
+    // удаление парка
+    Route::delete('/park_delete', 'ParkController@delete')->name('park_delete');
+    // показать все автомобили
+    Route::get('/all_trucks', 'TruckController@index')->name('truck_index');
+    // удалить автомобиль
+    Route::delete('/truck_delete', 'TruckController@delete')->name('truck_delete');
+    // получить информацию об автомобиле (по госномеру)
+    Route::get('/truck', 'TruckController@info')->name('truck_info');
+});
 
-Route::get('/park_edit/{id?}', 'ParkController@edit')->name('park_edit')->middleware('auth','can:all-parks');
+// Водитель
+Route::middleware(['auth','can:only-own-trucks'])->group(function () {
+    // показать автомобили пользователя-водителя
+    Route::get('/my_trucks', 'TruckController@show')->name('truck_show');
+    // создать/редактировать автомобиль
+    Route::get('/truck_edit/{id?}', 'TruckController@edit')->name('truck_edit');
+    // сохранение автомобиля
+    Route::post('/truck_update', 'TruckController@update')->name('truck_update');
+});
 
-Route::post('/park_update', 'ParkController@update')->name('park_update')->middleware('auth','can:all-parks');
 
-Route::delete('/park_delete', 'ParkController@delete')->name('park_delete')->middleware('auth','can:all-parks');
 
-Route::get('/all_trucks', 'TruckController@index')->name('truck_index')->middleware('auth', 'can:all-parks');
-
-Route::delete('/truck_delete', 'TruckController@delete')->name('truck_delete')->middleware('auth','can:all-parks');
-
-Route::get('/truck', 'TruckController@info')->name('truck_info')->middleware('auth','can:all-parks');
-
-Route::get('/my_trucks', 'TruckController@show')->name('truck_show')->middleware('auth', 'can:only-own-trucks');
-
-Route::get('/truck_edit/{id?}', 'TruckController@edit')->name('truck_edit')->middleware('auth', 'can:only-own-trucks');
-
-Route::post('/truck_update', 'TruckController@update')->name('truck_update')->middleware('auth', 'can:only-own-trucks');
 
